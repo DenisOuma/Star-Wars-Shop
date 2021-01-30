58feaf636291a87e20719401b289a8b4c616eb1d
Denis <dennisouma38@gmail.com>
2021-01-30 19:42:07 +0300

    Good Attempt

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// Login Function on Click
+
+var userObj = [
+    {
+        userName: 'dennis@gmail.com',
+        passWord: 'denis',
+    },
+
+    {
+        userName: 'kylee@gmail.com',
+        passWord: 'denis'
+    }
+];
+
+function loadHomePage(e) {
+    var userName = document.getElementById('userName').value
+    var passWord = document.getElementById('passWord').value
+
+    for (var i = 0; i < userObj.length; i++) {
+        if((userName === userObj[i].userName) && (passWord === userObj[i].passWord)) {
+            window.open("home.html");
+        }
+    }
+
+}
