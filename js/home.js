5b8565600023c456a3fc58c5a8e281c72577838e
Denis <dennisouma38@gmail.com>
2021-02-01 00:20:25 +0300

    Description secton Finished

---
+++ js/home.js
@@ -0,0 +1,66 @@
+var SearchBtn = document.getElementById('btn').addEventListener("click",getShip);
+var con = 0;
+var cardDiv = document.getElementById("cardDiv");
+let page = 2;
+let list = ""
+
+
+async function getShip(url){
+    try {
+        list = ""
+        console.log(url)
+       const res = await fetch(url)
+       let jsonData = await res.json()
+       console.log(jsonData)
+       jsonData.results.forEach( (ship) => {
+           console.log(ship.name)
+           list += `
+           
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="img/starwar.jpg" alt="" class="card-img-top">
+                        <div class="card-body">
+                        <h5>${ship.name}</h5>
+                        <h5>${ship.cost_in_credits}</h5>
+                        <h5>length</h5>
+                        <button type="button" class="btn btn-success" ><i class="fas fa-cart-plus"></i>Detail</button>
+                        </div>
+                        </div>
+                        </div>
+                `
+       })
+       cardDiv.innerHTML = list
+       document.getElementById("next").addEventListener("click",function(){
+           list = ""
+           jsonData.results = []
+           console.log(jsonData.results)
+           getShip(jsonData.next)
+       })
+       document.getElementById("prev").addEventListener("click",function(){
+           list = ""
+           jsonData.results = []
+           console.log(jsonData.results)
+           getShip(jsonData.previous)
+       })
+       if(!jsonData.previous){
+           document.getElementById("prev").disabled = true
+       }else{
+           document.getElementById("prev").disabled = false
+       }
+       if(!jsonData.next){
+           document.getElementById("next").disabled = true
+       }else{
+           document.getElementById("next").disabled = false
+       }
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+getShip(`https://swapi.dev/api/starships/`)
+
+
+
+
+
