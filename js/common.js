4104ab7f2748b60b89f3e5bffd48e893a91a3081
Denno31 <denniskyn80@gmail.com>
2021-02-01 09:10:19 +0300

    search by name feature

--- js/common.js
+++ js/common.js
@@ -1,13 +1,18 @@
-async function searchShip(){
-alert()
-    let searchTerms = document.getElementById("search").value;
-    console.log(searchTerms)
-    const res = await fetch(`https://swapi.dev/api/starships/?search=${searchTerms}`)
-    const jsonData = await res.json();
-    console.log('here')
-        jsonData.results.forEach( (ship) => {
-           console.log(ship.name)
-           list += `
+let myList = ''
+async function searchShip() {
+  myList = ''
+  let searchTerms = document.getElementById('search').value
+  searchTerms = searchTerms.replace(' ', '%20')
+  console.log(`https://swapi.dev/api/starships/?search=${searchTerms}`)
+  try {
+    const res = await fetch(
+      `https://swapi.dev/api/starships/?search=${searchTerms}`,
+    )
+    const jsonData = await res.json()
+    console.log(jsonData)
+    jsonData.results.forEach((ship) => {
+      console.log(ship.name)
+      myList += `
            
                 <div class="col-md-3">
                     <div class="card">
@@ -15,12 +20,19 @@ alert()
                         <div class="card-body">
                         <h5>${ship.name}</h5>
                         <h5>${ship.cost_in_credits}</h5>
-                        <h5>length</h5>
+                        <h5>${ship.length}</h5>
                         <button id="details" type="button" onClick="window.open('/productdetail.html?url=${ship.url}')" class="btn btn-success" ><i class="fas fa-cart-plus"></i>Detail</button>
                         </div>
                         </div>
                         </div>
                 `
-       })
+    })
+    cardDiv.innerHTML = myList
+  } catch (err) {
+    console.log(err)
+  }
 }
-document.getElementById("btn-search").addEventListener("click",searchShip)
+document.getElementById('btn-search').addEventListener('click', function (e) {
+  e.preventDefault()
+  searchShip()
+})
